592e6d52f8ba5a834786c752358ee520df1ca117
Izripov-Yusuf <izripov1998@gmail.com>
2020-01-27 19:27:50 +0300

    Усложнённое задание, 3 урок

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-let num = 266219;
-let res = 1;
-num.toString().split('').forEach(function (el) {
-  res *= el;
-});
-console.log(res);
+'use strict';
 
-console.log((res ** 3).toString().substring(0, 2));
+// Решение с помощью if
+let lang = prompt('Выберите язык, введя "ru" или "en"');
+if (lang === 'ru') {
+  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+
+// Решение с помощью switch
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    break;
+
+  case 'en':
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+    break;
+  default:
+    console.log('Не знаю такой язык(');
+    break;
+}
+
+// Решение с помощью многомерного массива
+let arr = {
+  'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+};
+console.log(arr[lang]);
+
+// Вторая часть усложнённого урока
+let namePerson = prompt('Ваше имя?');
+let checkNamePerson = namePerson === 'Артем' ? console.log('Директор') : (namePerson === 'Максим') ?
+console.log('Преподаватель') : console.log('Студент');
