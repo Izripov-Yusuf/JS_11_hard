10baff023f8224937f1693a08e52f97160840369
Izripov-Yusuf <izripov1998@gmail.com>
2020-01-28 21:02:00 +0300

    Усложнённое задание, 4 урок

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
 'use strict';
 
-// Решение с помощью if
-let lang = prompt('Выберите язык, введя "ru" или "en"');
-if (lang === 'ru') {
-  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} else if (lang === 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+let str = '    This is string This is string This is string This is string This is string This is string    ';
+function func(str) {
+  if (typeof(str) === 'string') {
+    console.log(str.trim());
+    if (str.trim().length > 30) {
+      console.log(str.trim().substring(0, 30) + '...');
+    }
+  } else {
+    console.log('Введите строку');
+  }
 }
-
-// Решение с помощью switch
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    break;
-
-  case 'en':
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    break;
-  default:
-    console.log('Не знаю такой язык(');
-    break;
-}
-
-// Решение с помощью многомерного массива
-let arr = {
-  'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-};
-console.log(arr[lang]);
-
-// Вторая часть усложнённого урока
-let namePerson = prompt('Ваше имя?');
-let checkNamePerson = namePerson === 'Артем' ? console.log('Директор') : (namePerson === 'Максим') ?
-console.log('Преподаватель') : console.log('Студент');
+func(str);
