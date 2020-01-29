4cca251abbfb4329c7e2401cfb5b2a2d4ecc8a78
Izripov-Yusuf <izripov1998@gmail.com>
2020-01-29 22:01:02 +0300

    Усложнённое пятое задание

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 'use strict';
 
-let str = '    This is string This is string This is string This is string This is string This is string    ';
-function func(str) {
-  if (typeof(str) === 'string') {
-    console.log(str.trim());
-    if (str.trim().length > 30) {
-      console.log(str.trim().substring(0, 30) + '...');
-    }
-  } else {
-    console.log('Введите строку');
+// Первое задание
+let arr = ['2678', '7777', '555', '444', '333', '222', '40456'];
+
+arr.forEach((e) => {
+  if (e[0] === '2' || e[0] === '4') {
+    console.log(`<br>${e}`);
   }
+});
+
+// Второе задание
+let x = 100;
+
+nextNumber:
+for (let i = 2; i <= x; i++) {
+
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      continue nextNumber;
+    }
+  }
+
+  console.log(i);
 }
-func(str);
