a7e895423f775fe44baec55623086bdb5d563073
Izripov-Yusuf <izripov1998@gmail.com>
2020-02-05 17:24:00 +0300

    Исправил склонения минут и секунд

--- script.js
+++ script.js
@@ -12,23 +12,20 @@ let date = new Date(),
   fullYear = date.toLocaleDateString(),
   fullTime = date.toLocaleTimeString();
 
-function num2str(n, textForms) {
+function num3str(n, textForms) {
   n = Math.abs(n) % 100; let n1 = n % 10;
   if (n > 10 && n < 20) { return textForms[2]; }
-  if (n1 > 1 && n1 < 5) { return textForms[1]; }
-  if (n1 === 1) { return textForms[0]; }
+  if (n1 > 1 && n1 < 5) { return textForms[0]; }
+  if (n1 === 1) { return textForms[1]; }
   return textForms[2];
 }
-  function checkTime(i) {
+function checkTime(i) {
     if (i < 10) {
       i = "0" + i;
     }
     return i;
   }
-  /* function time() {
-    return document.write(`${weekday[date.getDay()]}, ` + `${day} ` + month[date.getMonth()] + ` ${year} года, ` + `${checkTime(hours)} ${num2str(1, ['часов', 'час', 'часа'])} ` + `${checkTime(minutes)} минут ` + `${checkTime(seconds)} секунд`);
-  } */
-document.write('Сегодня ' + `${weekday[date.getDay()]}, ` + `${day} ` + month[date.getMonth()] + ` ${year} года, ` + `${checkTime(hours)} ${num2str(1, ['часов', 'час', 'часа'])} ` + `${checkTime(minutes)} минут ` + `${checkTime(seconds)} секунд`);
+document.write('Сегодня ' + `${weekday[date.getDay()]}, ` + `${day} ` + month[date.getMonth()] + ` ${year} года, ` + `${checkTime(hours)} ${num3str(checkTime(hours), ['часа', 'час', 'часов'])} ` + `${checkTime(minutes)} ${num3str(checkTime(minutes), ['минуты', 'минута', 'минуты'])} ` + `${checkTime(seconds)} ${num3str(checkTime(seconds), ['секунды', 'секунда', 'секунд'])}`);
 document.write(`<br>${fullYear} - ` + `${fullTime}`);
 
 
