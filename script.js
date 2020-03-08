80925c54b8aa92e6ff37736bb503dcc77f110d3a
Izripov-Yusuf <izripov1998@gmail.com>
2020-03-08 23:50:52 +0300

    Исправил баги с консолью

--- script.js
+++ script.js
@@ -4,31 +4,47 @@ document.addEventListener('DOMContentLoaded', () => {
   const select = document.getElementById('cars'),
         output = document.getElementById('output');
 
-  select.addEventListener('change', () => {
-    const getData = (url) => {
-      return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.open('GET', url);
-        request.setRequestHeader('Content-type', 'application/json');
-        request.addEventListener('readystatechange', () => {
-          if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            resolve(data);
+  const getData = (url) => {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.setRequestHeader('Content-type', 'application/json');
+      request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) {
+          return;
+        }
+        if (request.status === 200) {
+          const data = JSON.parse(request.responseText);
+          const outputSuccessMessage = () => {
             data.cars.forEach(item => {
               if (item.brand === select.value) {
-                const { brand, model, price } = item;
+                const {
+                  brand,
+                  model,
+                  price
+                } = item;
                 output.innerHTML = `Тачка ${brand} ${model} <br>
                   Цена: ${price}$`;
               }
             });
-          } else {
-            reject(request.statusText);
+          };
+          resolve(outputSuccessMessage);
+        } else {
+          const outputErrorMessage = () => {
             output.innerHTML = 'Произошла ошибка';
-          }
-        });
-        request.send();
+          };
+          reject(outputErrorMessage());
+        }
       });
-    };
-    getData('./cars.json');
+      request.send();
+    });
+  };
+
+  select.addEventListener('change', () => {
+    getData('./cars.json')
+      .then(outputSuccessMessage => {
+        outputSuccessMessage();
+      })
+      .catch(outputErrorMessage => outputErrorMessage);
   });
 });
